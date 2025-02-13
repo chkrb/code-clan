14eecbdbc24a6c6c8c1a82e00f1f6ffcca7daa15
Kaustabh Chakraborty <chakraborty.kaustabh@protonmail.com>
2025-02-13 23:52:31 +0530

    misc changes
    * inverted colors of dot matrix tile
    * viewport is fixed to 1920x1080 and is letterboxed
    * enable wayland support for linux
    * add sample interfacing code for pin grid and sample pin widgets

---
+++ scripts/pin_generate.gd
@@ -0,0 +1,10 @@
+extends GridContainer
+
+# Apparent screen size: 1920x1080
+# Number of pins: 32x18
+var pin_widgets = []
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
